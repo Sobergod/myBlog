4dd1e5d4fcd435fd4a2b900eacd6ccbfb01ccf7a
sober_e1@qq.com <sober_e1@qq.com>
2018-03-04 19:11:55 +0800

    大框来了

---
+++ js/common.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+    $(function() {
+        GoTop.init(".go").addGotoTop();
+    });
+});
+
+var GoTop = {
+    goBtn:null,
+    topHeight: 100,
+    mWindow:$(window),
+    init:function(config) {
+        this.goBtn = $(config);
+        this.bind();
+        return this;
+    },
+
+    bind:function() {
+        var self = this;
+        this.goBtn.on("click",function() {
+            self.gotoTop();
+        })
+    },
+
+    gotoTop: function() {
+        $('html , body').animate({scrollTop: 0},'slow');
+    },
+
+    checkTop:function() {
+        if(this.mWindow.scrollTop() > this.topHeight) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+
+    addGotoTop:function() {
+        var self = this;
+        this.mWindow.scroll(function() {
+            if(self.checkTop()) {
+                self.goBtn.addClass("goto-top");
+            } else {
+                self.goBtn.removeClass("goto-top");
+            }
+        });
+    },
+
+
+
+}
